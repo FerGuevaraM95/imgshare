7b514bbd2b225ac88a6e366bee8c506dac3ba2de
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-12-28 15:32:05 -0600

    (Add):Conecxion con la base de datos [database.js index.js keys.js]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const config = require('./server/config');
 
+// database
+require('./database');
+
+// starting the server
 const app = config(express());
+
 
 app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
