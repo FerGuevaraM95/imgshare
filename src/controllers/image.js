f62fac1aab76c202592ca55692c382f69b1c5ffd
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-12-29 20:38:37 -0600

    (Add):Rutas iniciales [routes home.js image.js]

---
+++ src/controllers/image.js
@@ -0,0 +1,23 @@
+ctrl = {};
+
+ctrl.index = (req, res) => {
+    
+}
+
+ctrl.creare = (req, res) => {
+
+}
+
+ctrl.like = (req, res) => {
+
+}
+
+ctrl.comment = (req, res) => {
+
+}
+
+ctrl.remove = (req, res) => {
+
+}
+
+module.exports = ctrl;
