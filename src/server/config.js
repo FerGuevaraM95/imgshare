55305c3a28fa52431c0bbdcc9147b5fbdd58507b
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-01-19 15:03:47 -0600

    (Add):Plantilla inicial [public views]

--- src/server/config.js
+++ src/server/config.js
@@ -12,11 +12,11 @@ module.exports = app => {
 
     // Setings
     app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(__dirname, 'views'));
+    app.set('views', path.join(__dirname, '../views'));
     app.engine('.hbs', exphbs({
         defaultLayout: 'main',
         partialsDir: path.join(app.get('views'), 'partials'),
-        layoutsDir: path.join(app.get('views'), 'layout'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
         extname: '.hbs',
         helpers: require('./helpers')
     }));
@@ -32,7 +32,7 @@ module.exports = app => {
     routes(app);
 
     //Static files
-    app.use('public', express.static(path.join(__dirname, '../public')));
+    app.use('/public', express.static(path.join(__dirname, '../public')));
 
     // Errorhandlers
     if ('development' === app.get('env')) {
