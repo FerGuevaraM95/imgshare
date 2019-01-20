0aff8ee1ac45ff13146417b0e420731504533961
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-01-20 00:15:59 -0600

    (Add):Subir imagenes al servidor [controllers/image.js helpers]

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,25 @@
+const path = require('path');
+const { randomName } = require('../helpers/libs');
+const fs = require('fs-extra');
+
 ctrl = {};
 
 ctrl.index = (req, res) => {
     
 }
 
-ctrl.creare = (req, res) => {
+ctrl.create = async (req, res) => {
+    const imgUrl = randomName();
+    console.log(imgUrl);
+    const imageTempPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
+    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        await fs.rename(imageTempPath, targetPath);
+    }
+
+    res.send('works!');
 }
 
 ctrl.like = (req, res) => {
