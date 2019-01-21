f38a77dba4354687f5eb3a60696d15c55ca760ee
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-01-20 23:31:23 -0600

    (Add): Informacion de la imagen en la DB [models controlles/image.js]

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -2,6 +2,8 @@ const path = require('path');
 const { randomName } = require('../helpers/libs');
 const fs = require('fs-extra');
 
+const { Image } = require('../models');
+
 ctrl = {};
 
 ctrl.index = (req, res) => {
@@ -15,8 +17,17 @@ ctrl.create = async (req, res) => {
     const ext = path.extname(req.file.originalname).toLowerCase();
     const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
-    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
         await fs.rename(imageTempPath, targetPath);
+        const newImage = new Image({
+            title: req.body.title,
+            filename: imgUrl + ext,
+            description: req.body.description
+        });
+        const imageSaved = await newImage().save();
+    } else {
+        await fs.unlink(imageTempPath);
+        res.status(500).json({error: 'Only Images are allowed'})
     }
 
     res.send('works!');
